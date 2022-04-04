af7c6749a33d613842822570e9b486b77a398097
nbross <nbross2001@gmail.com>
2022-04-04 15:53:59 -0400

    added psuedo code in js for a mock road map

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// add event listener for search button 
+// add ability for users to see their search history and have that logged on screen
+// have a weather function with third party api and variables that create their own elements representing todays weather
+// have a forecast function that creates a 5-day forecast with the api
