e072e16f35726fbcb003678da5aa05d96b24a516
nbross <nbross2001@gmail.com>
2022-04-04 16:14:40 -0400

    added search button click listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,16 @@
 // add ability for users to see their search history and have that logged on screen
 // have a weather function with third party api and variables that create their own elements representing todays weather
 // have a forecast function that creates a 5-day forecast with the api
+
+// start with a .ready function for loading the data on launch
+$(document).ready(function () {
+    // search button click listener
+    $("#search-button").on("click", function () {
+        // this grabs values inputted into the search bar
+        var searchTerm = $("#search-bar").val();
+        // just incase there is no value inputted 
+        $("#search-bar").val("");
+        weatherFunction(searchTerm);
+        forecastFunction(searchTerm);
+    });
+});
